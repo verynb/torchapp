547215db44b31f9fc504a30849ef10c87ad53031
nicole.fu <nicole.fu@60living.com>
2017-07-31 20:15:32 +0800

    家访录入加入图片例子     批次修改编辑省市

--- js/plug/business.js
+++ js/plug/business.js
@@ -456,3 +456,30 @@ function showImg(obj) {
 	$(li).attr("data-index", beginIndex);
 	common.loadPage(li);
 }
+//点击浏览图片
+function showImgNow(obj) {
+	event.stopPropagation();
+	var aSrc = [];
+	var beginIndex = 0;
+	var iSrc = $(obj).attr("src");
+	var html = '';
+	var li = document.createElement("li");
+	$(li).attr("data-tkey", "scanImg");
+	$(li).attr("data-dir", "general");
+	$(li).attr("data-transition", "fade");
+	$.each($(obj).parent().find("img"), function(index, val) {
+		aSrc.push($(val).attr("src"));
+		if($(val).attr("src") == iSrc) {
+			beginIndex = index;
+		}
+	});
+	for(var i = 0; i < aSrc.length; i++) {
+		if(i != 0) {
+			html += ",";
+		}
+		html += aSrc[i];
+	}
+	$(li).attr("data-img", html);
+	$(li).attr("data-index", beginIndex);
+	common.loadPage(li);
+}
